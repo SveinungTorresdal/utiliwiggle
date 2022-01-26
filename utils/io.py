c04c4c8534b149d9a632acf90ce728e452068b14
pixelsuchter <76219112+pixelsuchter@users.noreply.github.com>
2022-01-26 21:26:44 +0100

    simplified code and changed function to look for file extension

--- utils/io.py
+++ utils/io.py
@@ -1,15 +1,14 @@
 import os
 
-def find(path, reg):
-    print(reg)
-    files = list()
+
+def get_filepaths_by_extension(path: str, file_extension: str):
+    print(file_extension)
+    files = []
 
     for (dirpath, dirnames, filenames) in os.walk(path):
-        files += [os.path.join(dirpath, file) for file in filenames]
-        
-    output = list(filter(lambda x: reg in x, files))
+        files.extend([os.path.join(dirpath, file) for file in filenames if file.endswith(file_extension)])
 
-    return output
+    return files
 
-if __name__== "__main__":
+if __name__ == "__main__":
     print('Wrong file. Run main.py.')

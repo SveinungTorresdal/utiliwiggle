76d7803edd32e6a98700e4ee74f13fded617e426
pixelsuchter <76219112+pixelsuchter@users.noreply.github.com>
2022-01-28 23:09:00 +0100

    started work on position slots

---
+++ utils/screen_math_helper.py
@@ -0,0 +1,42 @@
+"""
+Utility functions and classes to help create layouts
+"""
+from math import sqrt
+from typing import Union, List, Tuple
+
+
+class Slot:
+    """container class to wrap an OBS Object (e.g. image) with on-screen coordinates and rotation"""
+    x_coord: int  # the X coordinate of the object in pixel
+    y_coord: int  # the Y coordinate of the object in pixel
+    rotation: int  # rotation of the objects in degree
+    image_object: object
+
+    target_x: int
+    target_y: int
+    target_rotation: int
+
+    def __init__(self, x_coord: int, y_coord: int, rotation: int, image_object,
+                 target_x: Union[int, None] = None, target_y: Union[int, None] = None, target_rotation: Union[int, None] = None):
+        self.x_coord = x_coord
+        self.y_coord = y_coord
+        self.rotation = rotation
+        self.image_object = image_object
+        self.target_x = target_x if target_x is not None else self.x_coord  # assign targets if given, otherwise assign current position
+        self.target_y = target_y if target_y is not None else self.y_coord
+        self.target_rotation = target_rotation if target_rotation is not None else self.rotation
+
+    def set_target(self, target_x: Union[int, None] = None, target_y: Union[int, None] = None, target_rotation: Union[int, None] = None):
+        self.target_x = target_x if target_x is not None else self.x_coord  # assign targets if given, otherwise assign current position
+        self.target_y = target_y if target_y is not None else self.y_coord
+        self.target_rotation = target_rotation if target_rotation is not None else self.rotation
+
+    def get_distance(self) -> float:
+        d_x, d_y, d_rot = self.get_target_vector()
+        return sqrt(d_x**2 + d_y**2 + d_rot**2)
+
+    def get_target_vector(self) -> Tuple[int, int, int]:
+        delta_x = self.target_x-self.x_coord
+        delta_y = self.target_y-self.y_coord
+        delta_rot = self.target_rotation-self.rotation
+        return delta_x, delta_y, delta_rot
